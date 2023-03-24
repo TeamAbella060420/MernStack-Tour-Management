15d49229183858cd72cf75b57e5f03f3a991b567
Muhandis <Muhandis@wf.local>
2023-03-24 12:59:44 +0800

    add code

--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import '../styles/home.css'
+import { Container, Row, Col } from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from '../shared/Subtitle'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    {/* ==================== hero section start =================== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={'Know before you Go'}/>
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>Traveling opens the door to creating
+                   <span className="highlight">memories</span>
+                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe asperiores, quasi mollitia quos sit odit consequatur vitae quisquam eius nulla!</p>
+                </h1>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    {/* ==================== hero section end =================== */}
+    </>
   )
 }
 
