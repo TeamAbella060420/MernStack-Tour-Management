076fa2006937f8d9f8ce25628cd93990261ed9e9
Muhandis <Muhandis@wf.local>
2023-03-24 14:17:14 +0800

    add code

--- tour-management/frontend/src/shared/SearchBar.jsx
+++ tour-management/frontend/src/shared/SearchBar.jsx
@@ -8,21 +8,21 @@ const SearchBar = () => {
     <Col lg='12'>
         <div className="search__bar">
             <Form className='d-flex align-items-center gap-4'>
-                <FormGroup className='d-flex gap-3 form__froup form__group-fast'>
+                <FormGroup className='d-flex gap-3 form__group form__group-fast'>
                     <span><i class="ri-map-pin-line"></i></span>
                     <div>
                         <h6>Location</h6>
                         <input type="text" placeholder='Where are you going?' />
                     </div>
                 </FormGroup>
-                <FormGroup className='d-flex gap-3 form__froup form__group-fast'>
+                <FormGroup className='d-flex gap-3 form__group form__group-fast'>
                     <span><i class="ri-map-pin-time-line"></i></span>
                     <div>
                         <h6>Distance</h6>
                         <input type="number" placeholder='Distance k/m' />
                     </div>
                 </FormGroup>
-                <FormGroup className='d-flex gap-3 form__froup form__group-last'>
+                <FormGroup className='d-flex gap-3 form__group form__group-last'>
                     <span><i class="ri-group-line"></i></span>
                     <div>
                         <h6>Max People</h6>
