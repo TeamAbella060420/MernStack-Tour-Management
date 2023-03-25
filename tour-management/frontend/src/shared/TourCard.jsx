1f1698d762245c938ffa826f4d2707c2f17d5628
Muhandis <Muhandis@wf.local>
2023-03-25 16:07:06 +0800

    add code

--- tour-management/frontend/src/shared/TourCard.jsx
+++ tour-management/frontend/src/shared/TourCard.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Card, CardBody } from "reactstrap";
+import { Link } from 'react-router-dom'
 
 const TourCard = ({ tour }) => {
   const { id, title, photo, price, featured, avgRating, reviews } = tour;
 
-  return <div>TourCard</div>;
+  return (
+    <div className="tour__card">
+        <Card>
+            <div className="tour__img">
+                <img src="" alt="" />
+            </div>
+        </Card>
+    </div>
+  );
 };
 
 export default TourCard;
