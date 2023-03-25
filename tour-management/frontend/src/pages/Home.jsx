f78c3af5fa9432cb4ed62df3d3555e5ff1229980
Muhandis <Muhandis@wf.local>
2023-03-25 15:03:49 +0800

    add code

--- tour-management/frontend/src/pages/Home.jsx
+++ tour-management/frontend/src/pages/Home.jsx
@@ -1,48 +1,54 @@
-import React from 'react'
-import '../styles/home.css'
-import { Container, Row, Col } from 'reactstrap'
-import heroImg from '../assets/images/hero-img01.jpg'
-import heroImg02 from '../assets/images/hero-img02.jpg'
-import heroVideo from '../assets/images/hero-video.mp4'
-import worldImg from '../assets/images/world.png'
-import Subtitle from '../shared/Subtitle'
-import SearchBar from '../shared/SearchBar'
+import React from "react";
+import "../styles/home.css";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "../shared/Subtitle";
+import SearchBar from "../shared/SearchBar";
 
-import ServiceList from '../services/ServiceList'
+import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 
 const Home = () => {
   return (
     <>
-    {/* ==================== hero section start =================== */}
+      {/* ==================== hero section start =================== */}
       <section>
         <Container>
           <Row>
-            <Col lg='6'>
+            <Col lg="6">
               <div className="hero__content">
                 <div className="hero__subtitle d-flex align-items-center">
-                  <Subtitle subtitle={'Know Before You Go'}/>
+                  <Subtitle subtitle={"Know Before You Go"} />
                   <img src={worldImg} alt="" />
                 </div>
-                <h1>Traveling opens the door to creating{" "}
-                   <span className="highlight">memories</span>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight">memories</span>
                 </h1>
-                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe asperiores, quasi mollitia quos sit odit consequatur vitae quisquam eius nulla!</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Saepe asperiores, quasi mollitia quos sit odit consequatur
+                  vitae quisquam eius nulla!
+                </p>
               </div>
             </Col>
 
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box">
                 <img src={heroImg} alt="" />
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-4">
                 <video autoPlay loop>
-                  <source src={heroVideo} alt="" type="video/mp4"/>
+                  <source src={heroVideo} alt="" type="video/mp4" />
                 </video>
               </div>
             </Col>
-            <Col lg='2'>
+            <Col lg="2">
               <div className="hero__img-box mt-5">
                 <img src={heroImg02} alt="" />
               </div>
@@ -52,13 +58,13 @@ const Home = () => {
           </Row>
         </Container>
       </section>
-    {/* ==================== hero section end =================== */}
+      {/* ==================== hero section end =================== */}
       <section>
         <Container>
           <Row>
-            <Col lg='3'>
+            <Col lg="3">
               <h5 className="services__subtitle">What we serve</h5>
-              <h2 className='services__title'>We offer our best servises</h2>
+              <h2 className="services__title">We offer our best servises</h2>
             </Col>
 
             <ServiceList />
@@ -66,22 +72,21 @@ const Home = () => {
         </Container>
       </section>
 
-    {/* ==================== featured tour section start =================== */}
-    <section>
-      <Container>
-        <Row>
-          <Col lg='12' className='mb-5'>
-            <Subtitle subtitle={'Explore'}/>
-            <h2 className='featured__tour-title'>Our featured tours</h2>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-    {/* ==================== featured tour section end =================== */}
-
-
+      {/* ==================== featured tour section start =================== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5">
+              <Subtitle subtitle={"Explore"} />
+              <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+      {/* ==================== featured tour section end =================== */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ tour-management/frontend/src/shared/TourCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TourCard = ({ tour }) => {
+  const { id, title, photo, price, featured, avgRating, reviews } = tour;
+
+  return <div>TourCard</div>;
+};
+
+export default TourCard;
