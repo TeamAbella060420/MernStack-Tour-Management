454423f98d94b9dc385d596fe65a0ae983b12432
Muhandis <Muhandis@wf.local>
2023-03-28 13:26:08 +0800

    add code

--- tour-management/frontend/src/components/Testimonial/Testimonials.jsx
+++ tour-management/frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,9 +1,17 @@
 
 import React from 'react'
+import Slider from 'react-slick'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <Slider>
+      <div className="testimonial py-4 px-3">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Provident dolorum, culpa sed illo iure et qui nesciunt nisi
+          laudantium delectus distinctio ipsum reiciendis? Illum maxime
+          iusto provident optio ea a.</p>
+      </div>
+    </Slider>
   )
 }
 
