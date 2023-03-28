091b29ede85d42b7b7af27fd752825408ebc93d2
Muhandis <Muhandis@wf.local>
2023-03-28 16:06:27 +0800

    add code

---
+++ tour-management/frontend/src/shared/NewsLetter.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './newsletter.css'
+
+import { Container, Col, Row, } from 'reactstrap'
+import maleTourist from '../assets/images/male-tourist.png'
+
+const NewsLetter = () => {
+  return (
+    <section>
+        <Container>
+            <Row>
+                <Col lg='6'>
+                    <div className="newsletter__content">
+                        <h2>Subscribe now to get useful traveling information</h2>
+
+                        <div className="newsletter__input">
+                            <input type="email" placeholder='Enter your email' />
+                            <button className="btn newsletter__btn">Subscribe</button>
+                        </div>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi unde possimus laborum voluptatem voluptatibus nam!</p>
+                    </div>
+                </Col>
+                <Col lg='6'>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+  )
+}
+
+export default NewsLetter
