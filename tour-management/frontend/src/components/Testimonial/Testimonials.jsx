4dbec4e8fbb197ff400dd1645ec09eeb7251341c
Muhandis <Muhandis@wf.local>
2023-03-28 14:04:13 +0800

    add code

--- tour-management/frontend/src/components/Testimonial/Testimonials.jsx
+++ tour-management/frontend/src/components/Testimonial/Testimonials.jsx
@@ -6,8 +6,39 @@ import ava02 from '../../assets/images/ava-2.jpg'
 import ava03 from '../../assets/images/ava-3.jpg'
 
 const Testimonial = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+
+    responsive:[
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ]
+
+  }
+
   return (
-    <Slider>
+    <Slider {...settings}>
       <div className="testimonial py-4 px-3">
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -19,7 +50,40 @@ const Testimonial = () => {
         <div className='d-flex align-items-center gap-4 mt-3'>
           <img src={ava01} className='w-25 h-25 rounded-2' alt="" />
           <div>
-            <h5></h5>
+            <h6 className='mb-0 mt-3'>John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonial py-4 px-3">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Provident dolorum, culpa sed illo iure et qui nesciunt nisi
+          laudantium delectus distinctio ipsum reiciendis? Illum maxime
+          iusto provident optio ea a.
+        </p>
+
+        <div className='d-flex align-items-center gap-4 mt-3'>
+          <img src={ava02} className='w-25 h-25 rounded-2' alt="" />
+          <div>
+            <h6 className='mb-0 mt-3'>John Doe</h6>
+            <p>Customer</p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonial py-4 px-3">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Provident dolorum, culpa sed illo iure et qui nesciunt nisi
+          laudantium delectus distinctio ipsum reiciendis? Illum maxime
+          iusto provident optio ea a.
+        </p>
+
+        <div className='d-flex align-items-center gap-4 mt-3'>
+          <img src={ava03} className='w-25 h-25 rounded-2' alt="" />
+          <div>
+            <h6 className='mb-0 mt-3'>John Doe</h6>
+            <p>Customer</p>
           </div>
         </div>
       </div>
