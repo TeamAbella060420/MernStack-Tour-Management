d2bc656e6e68deb1ec040ff9c7e17a26f921f9cd
Muhandis <Muhandis@wf.local>
2023-03-28 13:36:23 +0800

    add code

--- tour-management/frontend/src/components/Testimonial/Testimonials.jsx
+++ tour-management/frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,15 +1,24 @@
 
 import React from 'react'
 import Slider from 'react-slick'
+import ava01 from '../../assets/images/ava-1.jpg'
+import ava02 from '../../assets/images/ava-2.jpg'
+import ava03 from '../../assets/images/ava-3.jpg'
 
 const Testimonial = () => {
   return (
     <Slider>
       <div className="testimonial py-4 px-3">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
           Provident dolorum, culpa sed illo iure et qui nesciunt nisi
           laudantium delectus distinctio ipsum reiciendis? Illum maxime
-          iusto provident optio ea a.</p>
+          iusto provident optio ea a.
+        </p>
+
+        <div className='d-flex align-items-center gap-4 mt-3'>
+          <img src={ava01} alt="" />
+        </div>
       </div>
     </Slider>
   )
