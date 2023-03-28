9c236bea7eaa02011563ca61b98edd3d413e7412
Muhandis <Muhandis@wf.local>
2023-03-28 16:32:00 +0800

    add code

--- tour-management/frontend/src/components/Footer/Footer.jsx
+++ tour-management/frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './footer.css'
 import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
