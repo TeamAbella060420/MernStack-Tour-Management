f151a26876a364a056cc96beae9172d96cbe5543
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-04-06 14:50:19 +0800

    add code

--- tour-management/frontend/src/components/Booking/Booking.jsx
+++ tour-management/frontend/src/components/Booking/Booking.jsx
@@ -20,6 +20,18 @@ const Booking = ({ tour, avgRating }) => {
         setCredentials(prev => ({...prev, [e.target.id]: e.target.value}))
     };
 
+    // send data to the server
+    const handleClick = e => {
+        e.preventDefault()
+
+        console.log(credentials, 'sss');
+    }
+
+    const serviceFee = 10 
+    const totalAmount = Number(price) * Number(credentials.Guest) + Number(serviceFee)
+    const totalPrice = Number(price) * Number(credentials.Guest)
+    const Guest = Number(credentials.Guest)
+
   return (
     <div className="booking">
         <div className="booking__top d-flex align-items-center justify-content-between">
@@ -32,7 +44,7 @@ const Booking = ({ tour, avgRating }) => {
         {/*=============== booking form start ==================*/}
         <div className="booking__form">
             <h5>Information</h5>
-            <Form className='booking__info-form'>
+            <Form className='booking__info-form' onSubmit={handleClick}>
                 <FormGroup>
                     <input 
                         type="text" 
@@ -74,19 +86,19 @@ const Booking = ({ tour, avgRating }) => {
         <div className="booking__bottom">
             <ListGroup>
                 <ListGroupItem className='border-0 px-0'>
-                    <h5 className='d-flex align-items-center gap-1'>$ {price} <i class="ri-close-line"></i> 1 person</h5>
-                    <span> ${price}</span>
+                    <h5 className='d-flex align-items-center gap-1'>$ {price} <i class="ri-close-line"></i> {Guest} person</h5>
+                    <span> ${totalPrice}</span>
                 </ListGroupItem>
                 <ListGroupItem className='border-0 px-0'>
                     <h5>Service charge</h5>
-                    <span> $10</span>
+                    <span> ${serviceFee}</span>
                 </ListGroupItem>
                 <ListGroupItem className='total border-0 px-0'>
                     <h5>Total</h5>
-                    <span> $109</span>
+                    <span> ${totalAmount}</span>
                 </ListGroupItem>
 
-                <Button className='btn primary__btn w-100 mt-4'>Book Now</Button>
+                <Button className='btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
             </ListGroup>
         </div>
         {/*=============== booking form bottom ==================*/}
