0fc8c0fbeb18cb25526d73721447f1f9aa343b41
TeamAbella060420 <teamabella@outlook.com>
2023-04-07 12:21:47 +0800

    add code

--- tour-management/frontend/src/pages/ThankYou.jsx
+++ tour-management/frontend/src/pages/ThankYou.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Container, Col, Row, Button } from 'reactstrap'
 import { Link } from 'react-router-dom'
+import '../styles/thank-you.css'
 
 const ThankYou = () => {
   return (
