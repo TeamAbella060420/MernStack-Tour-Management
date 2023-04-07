b5183bd3be6ae1e9c5e6b1d68aafb49a80a14b21
TeamAbella060420 <teamabella@outlook.com>
2023-04-07 12:19:00 +0800

    add code

--- tour-management/frontend/src/pages/ThankYou.jsx
+++ tour-management/frontend/src/pages/ThankYou.jsx
@@ -1,9 +1,19 @@
 
 import React from 'react'
+import { Container, Col, Row, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 const ThankYou = () => {
   return (
-    <div>ThankYou</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <i class="ri-checkbox-circle-line"></i>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
