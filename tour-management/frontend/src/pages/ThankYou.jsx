6ac98a4fdc038266279aec92a37b8ed81616547a
TeamAbella060420 <teamabella@outlook.com>
2023-04-07 12:32:27 +0800

    add code

--- tour-management/frontend/src/pages/ThankYou.jsx
+++ tour-management/frontend/src/pages/ThankYou.jsx
@@ -9,7 +9,7 @@ const ThankYou = () => {
     <section>
       <Container>
         <Row>
-          <Col lg='12'>
+          <Col lg='12' className='pt-5 text-center'>
             <div className="thank__you">
               <span>
                 <i class="ri-checkbox-circle-line"></i>
