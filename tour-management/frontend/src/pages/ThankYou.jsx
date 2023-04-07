93f9850d825b54907b318fbcf88d89737fc2ecce
TeamAbella060420 <teamabella@outlook.com>
2023-04-07 12:27:04 +0800

    add code

--- tour-management/frontend/src/pages/ThankYou.jsx
+++ tour-management/frontend/src/pages/ThankYou.jsx
@@ -10,7 +10,15 @@ const ThankYou = () => {
       <Container>
         <Row>
           <Col lg='12'>
-            <i class="ri-checkbox-circle-line"></i>
+            <div className="thank__you">
+              <span>
+                <i class="ri-checkbox-circle-line"></i>
+              </span>
+              <h1 className="mb-3 fw-semibold">Thank You</h1>
+              <h3>Your tour is booked.</h3>
+
+              <Button className='btn primary__btn w-25'><Link to='/home'>Back to Home</Link></Button>
+            </div>
           </Col>
         </Row>
       </Container>
