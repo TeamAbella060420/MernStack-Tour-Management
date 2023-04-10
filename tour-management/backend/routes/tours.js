32e952bbdd6ed710bc431f0eb7d07d59a275a932
TeamAbella060420 <teamabella@outlook.com>
2023-04-10 16:05:50 +0800

    Update tours.js

--- tour-management/backend/routes/tours.js
+++ tour-management/backend/routes/tours.js
@@ -1,9 +1,27 @@
 import express from 'express'
-import { createTour } from '../controllers/tourController.js'
+import {
+     createTour,
+     deleteTour,
+     getAllTour,
+     getSingleTour,
+     updateTour
+} from '../controllers/tourController.js'
 
 const router = express.Router()
 
 // create new tour
 router.post('/', createTour)
+
+// update tour
+router.put('/:id', updateTour)
+
+// delete new tour
+router.delete('/:id', deleteTour)
+
+// getSingle tour
+router.get('/:id', getSingleTour)
+
+// getAll tour
+router.get('/:id', getAllTour)
 
 export default router;
