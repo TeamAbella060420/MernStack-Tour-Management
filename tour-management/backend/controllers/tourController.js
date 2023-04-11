f14e4605ef8b551de263a8b819f658a948f6196d
TeamAbella060420 <teamabella@outlook.com>
2023-04-11 18:36:09 +0800

    Update tourController.js

--- tour-management/backend/controllers/tourController.js
+++ tour-management/backend/controllers/tourController.js
@@ -97,3 +97,34 @@ export const getAllTour = async (req,res) => {
         })
     }
 }
+
+// get tour by search
+
+export const getTourBySearch = async(req, res) => {
+
+    // here 'i' means case sensitive
+    const city = new RegExp(req.query.city, 'i') 
+    const distance = parseInt(req.query.distance)
+    const maxGroupSize = parseInt(req.query.maxGroupSize)
+
+    try {
+
+        // gte means greater than equal
+        const tours = await Tour.find({ city, distance: {$gte: distance},
+            maxGroupSize: {$gte:maxGroupSize}
+        })
+
+        res.status(200).json({
+            success: true,
+            message: 'Successful',
+            data: tours
+        })
+        
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: 'Not found'
+        })
+    }
+
+}
