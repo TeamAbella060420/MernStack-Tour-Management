ae17f6dcf90d5b9c15caaca43477cf82d3872b87
TeamAbella060420 <teamabella@outlook.com>
2023-04-11 19:46:53 +0800

    Create authController.js

---
+++ tour-management/backend/controllers/authController.js
@@ -0,0 +1,20 @@
+import User from '../models/User.js'
+
+
+// user registration
+export const register = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+// user login
+export const login = async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
