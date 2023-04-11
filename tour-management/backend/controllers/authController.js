9de7fa962f5edb44fbfd49c5c570e87f0145b5ef
TeamAbella060420 <teamabella@outlook.com>
2023-04-11 20:03:09 +0800

    Update authController.js

--- tour-management/backend/controllers/authController.js
+++ tour-management/backend/controllers/authController.js
@@ -1,12 +1,26 @@
 import User from '../models/User.js'
-
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 // user registration
 export const register = async(req, res) => {
     try {
-        
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            photo: req.body.photo
+        })
+        await newUser.save()
+        res.status(200).json({
+            success: true,
+            message: 'Successfully Created'
+        })
     } catch (error) {
-        
+        res.status(500).json({
+            success: false,
+            message: 'Failed to create. Try again.'
+        })
     }
 }
 
