41bd2830944a72e14589b4bac9d72642aa68fb49
TeamAbella060420 <teamabella@outlook.com>
2023-04-12 17:40:20 +0800

    Update users.js

--- tour-management/backend/routes/users.js
+++ tour-management/backend/routes/users.js
@@ -2,18 +2,18 @@ import express from 'express'
 import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/useController.js'
 const router = express.Router()
 
-import { verifyUser } from '../utils/verifyToken.js'
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js'
 
 // update User
-router.put('/:id', updateUser)
+router.put('/:id', verifyUser, updateUser)
 
 // delete new User
-router.delete('/:id', deleteUser)
+router.delete('/:id', verifyUser, deleteUser)
 
 // getSingle User
 router.get('/:id', verifyUser, getSingleUser)
 
 // getAll Users
-router.get('/', getAllUser)
+router.get('/', verifyAdmin, getAllUser)
 
 export default router;
