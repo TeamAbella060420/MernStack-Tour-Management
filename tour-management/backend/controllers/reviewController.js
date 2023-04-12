574b765c39d761a030d4d3d9095edf407d62bca6
TeamAbella060420 <teamabella@outlook.com>
2023-04-12 17:48:47 +0800

    Create reviewController.js

---
+++ tour-management/backend/controllers/reviewController.js
@@ -0,0 +1,13 @@
+import Tour from "../models/Tour.js"
+import Review from "../models/Review.js"
+
+export const createReview = async(req, res) => {
+
+    const tourId = req.params.tourId
+
+    try {
+        
+    } catch (error) {
+        
+    }
+}
