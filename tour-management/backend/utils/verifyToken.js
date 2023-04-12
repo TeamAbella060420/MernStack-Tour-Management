3d90adeea9d1fb1ba2b186f939b94d46dc6923fb
TeamAbella060420 <teamabella@outlook.com>
2023-04-12 15:53:18 +0800

    Update verifyToken.js

--- tour-management/backend/utils/verifyToken.js
+++ tour-management/backend/utils/verifyToken.js
@@ -1,1 +1,12 @@
 import jwt from 'jsonwebtoken';
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.accessToken
+
+    if(!token) {
+        return res.status(401).json({
+            success: false,
+            message: "You're not authorize"
+        })
+    }
+}
